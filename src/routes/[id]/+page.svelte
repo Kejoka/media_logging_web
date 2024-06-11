e8fe43d87a52533cf7b4afc09a1f053dd5f17ac1
Kenn Kaubukowski <kenn.kaubukowski@htwk-leipzig.de>
2024-06-11 09:18:22 +0200

    profiles and preferences implemented

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,76 @@
+<!-- src/routes/account/+page.svelte -->
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { SubmitFunction } from '@sveltejs/kit';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	export let data;
+	export let form;
+
+	let { session, supabase, profile } = data;
+	$: ({ session, supabase, profile } = data);
+
+	let profileForm: HTMLFormElement;
+	let loading = false;
+	let username: string = profile?.username ?? '';
+	let avatarUrl: string = profile?.avatar_url ?? '';
+
+	const handleSubmit: SubmitFunction = () => {
+		loading = true;
+		return async () => {
+			loading = false;
+		};
+	};
+
+	const handleSignOut: SubmitFunction = () => {
+		loading = true;
+		return async ({ update }) => {
+			loading = false;
+			update();
+		};
+	};
+</script>
+
+<div class="form-widget">
+	<form
+		class="form-widget"
+		method="post"
+		action="?/update"
+		use:enhance={handleSubmit}
+		bind:this={profileForm}
+	>
+		<div>
+			<label for="email">Email</label>
+			<input id="email" type="text" value={session.user.email} disabled />
+		</div>
+
+		<div>
+			<label for="username">Username</label>
+			<input id="username" name="username" type="text" value={form?.username ?? username} />
+		</div>
+
+		<div>
+			<input
+				type="submit"
+				class="button block primary"
+				value={loading ? 'Loading...' : 'Save Changes'}
+				disabled={loading}
+			/>
+		</div>
+	</form>
+
+	<form method="post" action="?/signout" use:enhance={handleSignOut}>
+		<div>
+			<button class="button block" disabled={loading}>Sign Out</button>
+		</div>
+	</form>
+	<button
+		type="button"
+		on:click={() => {
+			goto(`${$page.url.pathname}/movieswiper`);
+		}}
+	>
+		MovieSwiper
+	</button>
+</div>
