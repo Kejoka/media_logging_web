2a6678eb1bbab52756ae3168306914a1d61c1173
Kenn Kaubukowski <kenn.kaubukowski@htwk-leipzig.de>
2024-06-27 17:16:14 +0200

    pord build

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 				exclude: ['<all>']
 			},
 			platformProxy: {
-				// configPath: 'wrangler.toml',
+				configPath: 'wrangler.toml',
 				environment: undefined,
 				experimentalJsonConfig: false,
 				persist: false
