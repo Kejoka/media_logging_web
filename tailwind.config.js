90a9aaed9afa757b6c0b8b088a4390ac12a23001
Kenn <kenn.kaubukowski@gmx.de>
2026-02-07 12:08:31 +0100

    Switching to clean new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['light', 'dark'],
+		darkTheme: 'dark',
+		logs: true
 	}
 };
